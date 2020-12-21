76873e4db148642180595538e8746df6342a1239
celestino1987 <73525266+celestino1987@users.noreply.github.com>
2020-12-21 11:08:06 +0100

    Update DataUser.js

--- src/components/DataUser.js
+++ src/components/DataUser.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-
 import CardUser from '../components/CardUser'
 
 export const DataUser = () => {
@@ -10,7 +9,6 @@ export const DataUser = () => {
         <div className="cajaPadre animate__animated animate__fadeInLeft">
          <Link className="link-2" to={'/'}> <ArrowBackIcon /> </Link>
         <CardUser />
-        
         </div>
     )
 }
